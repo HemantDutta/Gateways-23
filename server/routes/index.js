57e07953f7ccf8d6b05702d8bf16bf09b29acb07
HemantDutta <hemantnorddutta2@gmail.com>
2023-08-31 20:00:11 +0530

    Create Base Server | Create React App

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Gateways 2023 Server' });
+});
+
+module.exports = router;
