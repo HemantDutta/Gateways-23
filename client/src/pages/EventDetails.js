9508c13d8d80166954fa4642e7a6dc4a1b0ab23d
Adhish Bahl <adhishbahl0@gmail.com>
2023-09-24 16:39:30 +0530

    event registration form deign change

--- client/src/pages/EventDetails.js
+++ client/src/pages/EventDetails.js
@@ -8,10 +8,8 @@ import { PublicNavbar } from '../components/PublicNavbar'
 function EventDetails() {
   return (
     <div>
-        {/* Importing global CSS */}
-        <link href="/assets/Gateways23.css" rel="stylesheet" type="text/css" />
         {/* Header */}
-        <PublicNavbar active="home"/>
+        <PublicNavbar active="events"/>
         {/* Header End */}
         {/* Container for event details and form */}
         <section className="event-container">
@@ -22,17 +20,19 @@ function EventDetails() {
             {/* Events Details SideBar End */}
             {/* Events Form SideBar */}
             <div className="event-form">
-                <div className="event-form-heading">Registration Form</div>
-                <form action="" method="" className='registration-form'>
-                    <input type="text" name="Name" id="name" placeholder='Participant Name' />
-                    <input type="email" name="email" id="email" placeholder='E-mail' />
-                    <input type="phone" name="phone" id="phone" placeholder='Phone Number' />
-                    <input type="text" name="college" id="college" placeholder='College Name' />
-                    <div className="event-btn-container">
-                        <button type='button' className="event-cta">Register</button>
-                        <div className="event-cta-bg"/>
-                    </div>
-                </form>
+                <div className="event-form-bg">
+                    <div className="event-form-heading">Registration Form</div>
+                    <form action="" method="" className='registration-form'>
+                        <input type="text" name="Name" id="name" placeholder='Participant Name' />
+                        <input type="email" name="email" id="email" placeholder='E-mail' />
+                        <input type="phone" name="phone" id="phone" placeholder='Phone Number' />
+                        <input type="text" name="college" id="college" placeholder='College Name' />
+                        <div className="event-btn-container">
+                            <button type='button' className="event-cta">Register</button>
+                            <div className="event-cta-bg"/>
+                        </div>
+                    </form>
+                </div>
             </div>
             {/* Events Form SideBar End */}
         </section>
